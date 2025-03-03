7ca6ab406e5a69a2455c3c3886de4fe73fac5b75
akki <akshat.guduru@gmail.com>
2025-03-03 17:39:30 -0500

    fix

--- nn.cpp
+++ nn.cpp
@@ -20,12 +20,6 @@ struct Activation {
     function<double(double)> derivative;
 };
 
-
-struct Activation {
-    function<double(double)> func;
-    function<double(double)> derivative;
-
-};
 double sigmoid(double x) {
     return 1.0 / (1.0 + exp(-x));
 }
