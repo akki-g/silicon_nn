f90b75583a32fb5eab056fb60f1e85ed2fc552ae
akki-g <akshat.guduru@gmail.com>
2025-03-10 13:48:52 -0400

    seg fault

--- build.sh
+++ build.sh
@@ -12,14 +12,14 @@ for src in "${SOURCES[@]}"; do
     obj="${src%.*}.o"
     OBJS+=("$obj")
     if [[ "$(uname)" == "Darwin" ]]; then
-        clang++ -O3 -march=native -ffast-math -std=c++17 -arch arm64 -I./include -c "$src" -o "$obj"
+        clang++ -O3 -fsanitize=address -fno-omit-frame-pointer -g -march=native -ffast-math -std=c++17 -arch arm64 -I./include -c "$src" -o "$obj"
     else
         g++ -O3 -march=native -ffast-math -std=c++17 -I./include -c "$src" -o "$obj"
     fi
 done
 
 if [[ "$(uname)" == "Darwin" ]]; then
-    clang++ -dynamiclib -arch arm64 -I./include -o ${OUTPUT_DIR}/libnn.dylib "${OBJS[@]}" -framework Metal -framework Foundation -Wl,-rpath,/usr/local/lib
+    clang++ -dynamiclib -arch arm64 -fsanitize=address -I./include -o ${OUTPUT_DIR}/libnn.dylib "${OBJS[@]}" -framework Metal -framework Foundation -Wl,-rpath,/usr/local/lib
 else
     g++ -shared -fPIC -I./include -o ${OUTPUT_DIR}/libnn.so "${OBJS[@]}" -Wl,-rpath,/usr/local/lib
 fi
