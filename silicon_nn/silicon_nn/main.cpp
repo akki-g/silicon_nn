db81a9fd8ae226756a52dd72b1777185e4ef4800
akki-g <akshat.guduru@gmail.com>
2025-03-12 15:38:52 -0400

    x-code proj

---
+++ silicon_nn/silicon_nn/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  silicon_nn
+//
+//  Created by Akshat Guduru on 3/12/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
